c2482c6c0d3fa94e94600d261bb4d3d11bd46108
paulo.araujo <paulo@eufalo.com>
2023-07-10 19:17:52 -0300

    Aplicando connectionString do banco de dados de teste

---
+++ Back/FreelaCashAPI/FreelaCash.Repository/Migrations/20230710221529_in.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreelaCash.Repository.Migrations
+{
+    public partial class @in : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
